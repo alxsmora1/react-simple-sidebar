b4376f98bd816f806724a37e5350bb29c7f51394
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2020-08-06 14:05:47 -0500

    Add NoMatch Component Add the class component NoMatch to handle the error 404. NoMatch render a div with two headers for show an error 404  message.

---
+++ src/components/NoMatch.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class NoMatch extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Error 404 </h1>
+        <h4>Page not Found</h4>
+      </div>
+    );
+  }
+}
