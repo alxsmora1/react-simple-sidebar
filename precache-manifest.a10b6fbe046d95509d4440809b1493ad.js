db26ca666040beb534e79d486b7c24002deb1c90
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2020-11-05 15:14:04 -0600

    Updates

---
+++ precache-manifest.a10b6fbe046d95509d4440809b1493ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25629aa0819b30e62b4d1194ff985ee1",
+    "url": "/react-simple-sidebar/index.html"
+  },
+  {
+    "revision": "142d01367e2efbe00820",
+    "url": "/react-simple-sidebar/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "440e308a698e4a9d6536",
+    "url": "/react-simple-sidebar/static/css/main.346e3c18.chunk.css"
+  },
+  {
+    "revision": "142d01367e2efbe00820",
+    "url": "/react-simple-sidebar/static/js/2.b781ddd5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react-simple-sidebar/static/js/2.b781ddd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "440e308a698e4a9d6536",
+    "url": "/react-simple-sidebar/static/js/main.15e93327.chunk.js"
+  },
+  {
+    "revision": "84857ae548e5ad9e7989",
+    "url": "/react-simple-sidebar/static/js/runtime-main.2711f4a7.js"
+  }
+]);
